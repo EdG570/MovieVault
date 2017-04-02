18aa6769da83e8bf2a1531ed7028d7ea55eddbf4
EdG570 <cole570@hotmail.com>
2017-04-02 16:02:30 -0400

    Added MovieReviews repo to DAL

---
+++ DAL/MovieReviewsDb.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+
+namespace DAL
+{
+    public class MovieReviewsDb
+    {
+        private MovieVaultEntities db;
+
+        public MovieReviewsDb()
+        {
+            db = new MovieVaultEntities();
+        }
+
+        public IEnumerable<MovieReview> GetAll()
+        {
+            return db.MovieReviews.ToList();
+        }
+
+        public MovieReview GetById(int id)
+        {
+            return db.MovieReviews.Find(id);
+        }
+
+        public void Insert(MovieReview review)
+        {
+            db.MovieReviews.Add(review);
+            Save();
+        }
+
+        public void Delete(int id)
+        {
+            var review = db.MovieReviews.Find(id);
+            db.MovieReviews.Remove(review);
+            Save();
+        }
+
+        public void Update(MovieReview review)
+        {
+            db.Entry(review).State = EntityState.Modified;
+            Save();
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
