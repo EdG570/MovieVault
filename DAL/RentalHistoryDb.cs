88aebbb5f151adad134357bcd2c83d899fac39e9
EdG570 <cole570@hotmail.com>
2017-04-02 16:06:16 -0400

    Added RentalHistory repo to DAL

---
+++ DAL/RentalHistoryDb.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+
+namespace DAL
+{
+    public class RentalHistoryDb
+    {
+        private MovieVaultEntities db;
+
+        public RentalHistoryDb()
+        {
+            db = new MovieVaultEntities();
+        }
+
+        public IEnumerable<RentalHistory> GetAll()
+        {
+            return db.RentalHistories.ToList();
+        }
+
+        public RentalHistory GetById(int id)
+        {
+            return db.RentalHistories.Find(id);
+        }
+
+        public void Insert(RentalHistory history)
+        {
+            db.RentalHistories.Add(history);
+            Save();
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
