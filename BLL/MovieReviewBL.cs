8d746ba0d6f33ca4abaea470ba3d7e86ab03f656
EdG570 <cole570@hotmail.com>
2017-04-02 17:28:17 -0400

    Added MovieReviewBL to BLL

---
+++ BLL/MovieReviewBL.cs
@@ -0,0 +1,45 @@
+﻿using BOL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class MovieReviewBL
+    {
+        private MovieReviewsDb db;
+
+        public MovieReviewBL()
+        {
+            db = new MovieReviewsDb();
+        }
+
+        public IEnumerable<MovieReview> GetAll()
+        {
+            return db.GetAll();
+        }
+
+        public MovieReview GetById(int id)
+        {
+            return db.GetById(id);
+        }
+
+        public void Insert(MovieReview review)
+        {
+            db.Insert(review);
+        }
+
+        public void Delete(int id)
+        {
+            db.Delete(id);
+        }
+
+        public void Update(MovieReview review)
+        {
+            db.Update(review);
+        }
+    }
+}
