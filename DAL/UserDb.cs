47daf34da6978caa6b58daa79e691e35d0adab6b
EdG570 <cole570@hotmail.com>
2017-04-02 15:47:22 -0400

    Added User repository to DAL

---
+++ DAL/UserDb.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+
+namespace DAL
+{
+    public class UserDb
+    {
+        private MovieVaultEntities db;
+
+        public UserDb()
+        {
+            db = new MovieVaultEntities();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return db.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public void Insert(User user)
+        {
+            db.Users.Add(user);
+            Save();
+        }
+
+        public void Delete(int id)
+        {
+            var user = db.Users.Find(id);
+            db.Users.Remove(user);
+            Save();
+        }
+
+        public void Update(User user)
+        {
+            db.Entry(user).State = EntityState.Modified;
+            Save();
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
