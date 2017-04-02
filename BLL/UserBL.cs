14434c6c369b3bed29ab7bed9861ef2b96b54abc
EdG570 <cole570@hotmail.com>
2017-04-02 17:21:44 -0400

    Added UserBL to BLL

---
+++ BLL/UserBL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using BOL;
+
+namespace BLL
+{
+    public class UserBL
+    {
+        private UserDb db;
+
+        public UserBL()
+        {
+            db = new UserDb();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return db.GetAll();
+        }
+
+        public User GetById(int id)
+        {
+            return db.GetById(id);
+        }
+
+        public void Insert(User user)
+        {
+            db.Insert(user);
+        }
+
+        public void Delete(int id)
+        {
+            db.Delete(id);
+        }
+
+        public void Update(User user)
+        {
+            db.Update(user);
+        }
+    }
+}
