8ec5020c1515b7d9f389393e8d5abaa724417eb6
EdG570 <cole570@hotmail.com>
2017-04-02 17:24:48 -0400

    Added MovieBL to BLL

---
+++ BLL/MovieBL.cs
@@ -0,0 +1,45 @@
+﻿using BOL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class MovieBL
+    {
+        private MovieDb db;
+
+        public MovieBL()
+        {
+            db = new MovieDb();
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return db.GetAll();
+        }
+
+        public Movie GetById(int id)
+        {
+            return db.GetById(id);
+        }
+
+        public void Insert(Movie movie)
+        {
+            db.Insert(movie);
+        }
+
+        public void Delete(int id)
+        {
+            db.Delete(id);
+        }
+
+        public void Update(Movie movie)
+        {
+            db.Update(movie);
+        }
+    }
+}
