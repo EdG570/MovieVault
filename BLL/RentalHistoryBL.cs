8e86ed902cb665c339a6dab6b87ecf1e9313e201
EdG570 <cole570@hotmail.com>
2017-04-02 17:31:47 -0400

    Added RentalHistory to BLL

---
+++ BLL/RentalHistoryBL.cs
@@ -0,0 +1,35 @@
+﻿using BOL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class RentalHistoryBL
+    {
+        private RentalHistoryDb db;
+
+        public RentalHistoryBL()
+        {
+            db = new RentalHistoryDb();
+        }
+
+        public IEnumerable<RentalHistory> GetAll()
+        {
+            return db.GetAll();
+        }
+
+        public RentalHistory GetById(int id)
+        {
+            return db.GetById(id);
+        }
+
+        public void Insert(RentalHistory history)
+        {
+            db.Insert(history);
+        }
+    }
+}
